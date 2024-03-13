8803759020f0656ee923105443f2edb43fd48272
AlgoApprent1ce <138270725+AlgoApprent1ce@users.noreply.github.com>
2024-03-13 23:19:30 +0800

    Add files via upload

--- berth.cpp
+++ berth.cpp
@@ -1,1 +1,65 @@
+#include <bits/stdc++.h>
 #include "berth.h"
+#include "robot.h"
+#include "data.h"
+//伪代码
+void berth::update_robot(){
+    if(robot_pullgoods);//如果有机器人在泊位内并且执行pull指令卸货
+    berth_value.push(robot.goods);//将当前机器人货物的价值存入队列
+}
+
+void berth::update_boat(){
+    if(boat_leave){//如果当前船只驶离泊位
+        boat_id=-1;
+        boat_occupy_time=transport_time;//根据当前泊位到虚拟点占用离开船只的时间（期间船只送货，不可用）
+    }
+    if(boat_arrive){
+        boat_id;//这里记录驶入泊位船只信息
+    }
+}
+
+void berth::load(){
+    if(cur_boat!=-1){//当前泊位有船
+        while(boat[]_current_capacity<=boat[]_max_capacity&&!berth_value.empty()){//如果当前船只还未装满,且仍有货物可装时
+            for(int i=1;i<=loading_speed;i++){//每一帧按照泊位速度装载货物
+                int temp_val=berth_value.top();
+                if(current_capacity+temp_val<=max_capacity){//轮船未满时就装载货物
+                    boat_val+=temp_val;
+                    berth_value.pop();
+                }
+                else break;//此时船装满了，停止装载（可能要返回一些指示信息？）
+            }
+        }
+    }
+}
+
+/*
+想法：当时间快结束时在轮船未装满时提前让其离开，在计时结束之前产生价值避免浪费
+*/
+
+/*
+你可以写一下泊位的货物存储，和当船来装货的时候的货物变化
+逻辑是机器人到泊位后放下货物，应该要存下货物的价值，存到一个队列里，
+然后船来装货的时候货物依次先进后出，装货是有cd的，就是多少帧搬一个，大概实现这些东西
+*/
+
+/*3.2轮船和泊位机制
+轮船和泊位之间有到达、装载、驶离三种动作。每一帧在泊位处将依次执行到达、驶
+离、装载三种动作。
+当两个轮船在同一帧到达同一泊位时，先输入指令的轮船先进入泊位。对于另一艘轮
+船，选手可以选择不输入指令，继续在泊位等待，当泊位空闲时到达泊位，也可以选
+择输入移动、运输指令，移动到其他泊位或者移动到虚拟点。
+由于不考虑轮船路径规划，我们假定泊位间的移动时间都是 500 帧(10s)，判题器将告
+知选手轮船从每个泊位运输产生价值的时间。选手可以随时操控轮船进行运输或移
+位。轮船结束运输产生价值的帧时刻，选手可以执行 ship 指令把轮船从虚拟点移动到
+一个空闲的泊位上。
+*/
+
+/*
+泊位数据是由 10 行 5 列数字组成。
+每一行五个整数 id, x, y, time, velocity 表示一个泊位。id(0 <= id < 10)为该泊位的唯一标
+号，(x,y)表示该泊位的左上角坐标,time(1 <= time <= 1000)表示该泊位轮船运输到虚拟
+点的时间(虚拟点移动到泊位的时间同)，即产生价值的时间，时间用帧数表示。
+Velocity(1 <= Velocity <= 5)表示该泊位的装载速度，即每帧可以装载的物品数，单位
+是：个。保证对于每次提交全部泊位到虚拟点时间和相同。
+*/
--- berth.h
+++ berth.h
@@ -1,13 +1,16 @@
 #ifndef BERTH_H
 #define BERTH_H
+#include <queue>
+using namespace std;
+
 
 class Berth
 {
 public:
-    int x;
+    int x;//泊位左上顶点的位置
     int y;
-    int transport_time;
-    int loading_speed;
+    int transport_time;//该泊位轮船运输到虚拟点的时间(虚拟点移动到泊位的时间同)，即产生价值的时间
+    int loading_speed;//Velocity(1 <= Velocity <= 5)表示该泊位的装载速度，即每帧可以装载的物品数，单位是：个
     int val; //当前泊位货物价值
     Berth(){}
     Berth(int x, int y, int transport_time, int loading_speed) {
@@ -16,6 +19,10 @@ public:
         this -> transport_time = transport_time;
         this -> loading_speed = loading_speed;
     }
+    int cur_boat;//当前停靠船只的id,没有船则为-1
+    queue<int> berth_value;//用来存储泊位当前存放货物价值
+    int move_time;//泊位之间移动的用时，都为500帧，暂时没用
+
 };
 
 #endif // BERTH_H
